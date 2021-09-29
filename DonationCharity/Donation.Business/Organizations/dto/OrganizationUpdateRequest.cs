370eafda8053866b499c0276f1f841a627130762
My-Heaven <tdse2k@gmail.com>
2021-09-29 16:56:46 +0700

    ...

---
+++ DonationCharity/Donation.Business/Organizations/dto/OrganizationUpdateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Donation.Business.Organizations.dto
+{
+    public class OrganizationUpdateRequest
+    {
+        public int OrganizationId { get; set; }
+        public string OrganizationName { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
